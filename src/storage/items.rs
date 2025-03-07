db966a2b64f5785e286b9902c207509638d37b5b
chai <chainot@hotmail.co.th>
2025-03-07 22:33:20 +0700

    bumb

--- src/storage/items.rs
+++ src/storage/items.rs
@@ -2,24 +2,3 @@ use crate::storage::database::Database;
 use crate::send::item::Item;
 use sqlx::{query_as, query};
 use sqlx::Error;
-
-pub async fn fetch_items(db: &Database) -> Result<Vec<Item>, Error> {
-    let items = query_as!(
-        Item,
-        "SELECT id, name, description FROM items"
-    )
-    .fetch_all(&db.pool)
-    .await?;
-    Ok(items)
-}
-
-pub async fn insert_item(db: &Database, item: &Item) -> Result<(), Error> {
-    query!(
-        "INSERT INTO items (name, description) VALUES ($1, $2) RETURNING id",
-        item.name,
-        item.description
-    )
-    .execute(&db.pool)
-    .await?;
-    Ok(())
-}
